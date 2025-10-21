fc55f3c421b765c6800f21ab373ab9b7f8af1b2e
arnaumeseguer <arnaumeseguer@iesebre.com>
2025-10-21 16:14:37 +0200

    millora ex 3.5

--- ExercicisAccesDades/Exercicis/Ex3.5.py
+++ ExercicisAccesDades/Exercicis/Ex3.5.py
@@ -1,13 +1,25 @@
-def pilesTriangulars(n):
-    llaunes = n
-    necessaries = 0
-    for i in range(0,n - 1):
-        necessaries += i + 1
-        if necessaries == llaunes:
-            print(f"{llaunes} és un nombre vàlid")
-            break
+def pilesTriangulars():
 
-        elif necessaries > llaunes:
-            print(f"{llaunes} No és un nombre vàlid")
+    llaunes = 0
+    necessaries = 0
+    while True:
+        necessaries = 0
+        llaunes = int(input())
+        if llaunes == 0:
             break
-pilesTriangulars(int(input()))
+        if llaunes == 2:
+            print(f"{llaunes} No és un nombre vàlid")
+
+        if llaunes == 1:
+            print(f"{llaunes} és un nombre vàlid")
+
+        for i in range(0,llaunes - 1):
+            necessaries += i + 1
+            if necessaries == llaunes:
+                print(f"{llaunes} és un nombre vàlid")
+                break
+
+            elif necessaries > llaunes:
+                print(f"{llaunes} No és un nombre vàlid")
+                break
+pilesTriangulars()
