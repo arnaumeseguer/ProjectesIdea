3e541b161fa7c5cd4f289a4ef721452092f0c0ea
arnaumeseguer <arnaumeseguer@iesebre.com>
2025-11-12 20:25:41 +0100

    joel

---
+++ PythonProject/NouBarris.py
@@ -0,0 +1,8 @@
+barris = ["Verdum", "Les Roquetes", "La Prosperitat", "La Guineueta", "Canyelles", "Ciutat Meridiana", "La Trinitat Nova", "La Trinitat Vella"]
+
+barri = input()
+
+if barri in barris:
+    print("SI")
+else:
+    print("NO")
